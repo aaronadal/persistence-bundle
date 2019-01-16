b745e1ef9047d7e804f31d13ed164d32f5c69f1e
Aarón Nadal <aaronadal.dev@gmail.com>
2019-01-16 09:30:00 +0100

    Alias for FQCN services.

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -16,8 +16,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('aaronadal_persistence');
+        $treeBuilder = new TreeBuilder('aaronadal_persistence');
+        // $rootNode = $treeBuilder->getRootNode();
 
         return $treeBuilder;
     }
